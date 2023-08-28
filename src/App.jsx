3eb3fcf1b411e75df400a71d28685ad90aa0413b
edupoly <info.edupoly@gmail.com>
2023-08-28 09:18:35 +0530

    sdfsdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 import Counter from './Counter';
-import Todolist from './Todolist';
-
 function App() {
   return (
     <div className="mybox">
       <h1>Welcome to Edupoly ReactJS Training</h1>
-      <Counter></Counter>
-      <Todolist></Todolist>
+      
+      <Counter s={100} i={10}></Counter>
+      <Counter s={200} i={5}></Counter>
+
     </div>
   );
 }
 
 export default App;
+
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 
-function Counter(){
+function Counter(props){
+    console.log(props)
     //state
-    var [count,setCount]=React.useState(0)
+    var [count,setCount]=React.useState(props.s)
     //logic
+    function inc(){
+        setCount(count+props.i)
+    }
+    function dec(){
+        setCount(count-props.i)
+    }
     //template
     return (
         <div className='mybox'>
             <h1>Counter:{count}</h1>
+            <button onClick={()=>{inc()}}>Increment</button>
+            <button onClick={()=>{dec()}}>Decrement</button>
         </div>
     )
 }
