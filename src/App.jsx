986876fd1063211b759f76264e9bd4e3d517951c
edupoly <info.edupoly@gmail.com>
2023-08-30 09:25:21 +0530

    sdfsdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import Counter from './Counter';
-import Todolist from './Todolist';
+import React from 'react';
 
 function App() {
+  var [x,setx] = React.useState(133);
+  var [y,sety] = React.useState(234);
+  React.useEffect(()=>{
+    console.log("absdf")
+  },[]);
+
+  React.useEffect(()=>{
+    console.log("x updated")
+  },[x]);
+  React.useEffect(()=>{
+    console.log("y updated")
+  },[y]);
+
+  React.useEffect(()=>{
+    console.log("balu")
+  });
   return (
     <div className="mybox">
       <h1>Welcome to Edupoly ReactJS Training</h1>
-      <Counter></Counter>
-      <Todolist></Todolist>
+      <h1>Counter:{x}</h1>
+      <h1>Counter:{y}</h1>
+      <button onClick={()=>{setx(x+1)}}>Increment X</button>
+      <button onClick={()=>{sety(y+1)}}>Increment Y</button>
     </div>
   );
 }
