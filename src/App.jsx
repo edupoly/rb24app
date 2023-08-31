f1c882102fa4b7e1ba3f00cecb12f8a934248fe2
edupoly <info.edupoly@gmail.com>
2023-08-31 08:59:09 +0530

    sdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import Counter from './Counter';
-import Todolist from './Todolist';
-import { useEffect } from 'react';
+
 
 function App() {
-  useEffect(()=>{
-    alert("App")
-  },[])
+
   return (
     <div className="mybox">
       <h1>Welcome to Edupoly ReactJS Training</h1>
-      <Counter></Counter>
-      <Todolist></Todolist>
     </div>
   );
   
