43d495dff521d03cc4d8966a1d4aaef039b1ee2a
edupoly <info.edupoly@gmail.com>
2023-09-01 08:37:26 +0530

    xcv

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-
-
 function App() {
-
   return (
     <div className="mybox">
       <h1>Welcome to Edupoly ReactJS Training</h1>
     </div>
   );
-  
 }
 
 export default App;
+/*
+  functional components
+  class
+  smart components
+    container components
+  dumb component
+    presentational component
+  Controlled Components
+  Uncontrolled Components
+
+*/
