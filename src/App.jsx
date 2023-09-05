0ef0af8b3d1b72bbaa7faa73c7c2d9d3de11c982
edupoly <info.edupoly@gmail.com>
2023-09-05 08:45:16 +0530

    xd

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,10 @@
-import React, { useEffect } from "react";
-import Address from "./Address";
-
+import React from "react";
 function App() {
-  var firstnameRef = React.useRef();
-  var lastnameRef = React.useRef();
-  var addressRef = React.useRef();
-
-  useEffect(()=>{
-    firstnameRef.current.focus();
-  },[])
-  function focusLastname(e){
-    if(e.key==="Enter"){
-      lastnameRef.current.focus();
-    }
-  }
-  function focusAddress(e){
-    if(e.key==="Enter"){
-      addressRef.current.focus();
-    }
-  }
+  
   return (
     <div className="mybox">
       <h1>Welcome to Edupoly ReactJS Training</h1>
-      <input type="text" ref={firstnameRef} onKeyUp={(ev)=>{focusLastname(ev)}}/>
-      <br />
-      <input type="text" ref={lastnameRef} onKeyUp={(ev)=>{focusAddress(ev)}}/>
-      <Address ref={addressRef}></Address>
+      
     </div>
   );
   
