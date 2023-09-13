1a57b78224a184c228cebd77f493dd931f3424d6
edupoly <info.edupoly@gmail.com>
2023-09-13 08:42:35 +0530

    xcv

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { Provider } from "react-redux";
+import store from './store/store'
+import Counter from "./Counter";
+import Todolist from "./Todolist";
 function App() {
   
   return (
-    <div className="mybox">
-      <h1>Welcome to Edupoly ReactJS Training</h1>
-      
-    </div>
+    <Provider store={store}>
+      <div className="mybox">
+        <h1>Welcome to Edupoly ReactJS Training</h1>
+        <Counter></Counter>
+        <Todolist></Todolist>
+      </div>
+    </Provider>
   );
   
 }
